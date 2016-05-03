ac3a13697c2bcbb9624a46e8e45f9f72a6c5201d
Stefano10 <momoloss10@gmail.com>
2016-05-03 10:28:59 -0300

    Inicio....

--- src/Jogada.java
+++ src/Jogada.java
@@ -1,3 +1,6 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 /**
  * 
  */
@@ -6,6 +9,31 @@
  * @author joao
  *
  */
-public class Jogada {
+public class Jogada extends UnicastRemoteObject implements IJogada {
+	
+	private static final long serialVersionUID = 12345113265432345L;
+	
+	protected Jogada() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void realizarJogada(int palpite, int quantPalitosJogados, int id) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean esperar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String divulgarResultado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
